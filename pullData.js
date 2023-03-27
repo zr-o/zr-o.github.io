3cf3312a6ebc571d8ae15562d90c5c922700cbe7
zr-o <60937705+zr-o@users.noreply.github.com>
2023-03-27 16:04:12 +0100

    Update pullData.js

--- pullData.js
+++ pullData.js
@@ -45,7 +45,7 @@ function handleResponse(response) {
 
 function handleData(data) {
     console.log(data);
-    document.getElementById("query-text").innerHTML = data.data.Media.title.english;
+    document.getElementById("query-text").innerHTML = data.data.Media.id;
 }
 
 function handleError(error) {
