af1a6352a7086dcf07ea372d211f8f9ab64f8754
zr-o <60937705+zr-o@users.noreply.github.com>
2023-03-27 16:32:42 +0100

    Fix query

--- pullData.js
+++ pullData.js
@@ -26,6 +26,10 @@ function runQuery()
                         .catch(handleError);
 }
 
+function changeText(data)
+{
+    document.getElementById("query-text").innerHTML = data.data.Media.title.english;
+}
 
 // Define the config we'll need for our Api request
 var url = 'https://graphql.anilist.co',
@@ -51,7 +55,7 @@ function handleResponse(response) {
 
 function handleData(data) {
     console.log(data);
-    document.getElementById("query-text").innerHTML = data.data.Media.id;
+    document.getElementById("query-text").onclick = changeText(data);
 }
 
 function handleError(error) {
