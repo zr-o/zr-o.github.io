d9a94196d4e60cdd778fcc99d112254ec9b3a066
zr-o <60937705+zr-o@users.noreply.github.com>
2023-05-04 15:09:10 +0100

    Update loginPage.js

--- loginPage.js
+++ loginPage.js
@@ -1,6 +1,7 @@
 function checkUrl()
 {
     var url = window.location;
+    console.log(url)
     var access_token = new URLSearchParams(url.hash).get('access_token');
     console.log(access_token);
 }
